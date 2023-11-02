098bebeb3f9b57d53e298177f9fdb4b20ea1fd47
Tiago Martins Pinto <55080281+ptiagomp@users.noreply.github.com>
2023-11-02 22:49:41 +0200

    herokuteste1

--- public/script.js
+++ public/script.js
@@ -1,11 +1,14 @@
+// Connect to the server's socket
 const socket = io();
 
+// Listen for state changes from the server
 socket.on('stateChange', (state) => {
   const toggleSwitch = document.getElementById('toggleSwitch');
   toggleSwitch.checked = state;
   document.body.style.backgroundColor = state ? 'white' : 'black';
 });
 
+// Send state changes to the server when the user toggles the switch
 document.getElementById('toggleSwitch').addEventListener('change', function() {
   socket.emit('toggleSwitch', this.checked);
 });
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const http = require('http');
-const path = require('path');
 const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the public directory
+app.use(express.static('public'));
 
+// This variable holds the state of the switch
 let switchState = false;
 
+// Set up socket connection
 io.on('connection', (socket) => {
+  // Emit the current state to the newly connected client
   socket.emit('stateChange', switchState);
 
+  // Listen for switch toggles and broadcast the change to all clients
   socket.on('toggleSwitch', (state) => {
     switchState = state;
-    io.emit('stateChange', switchState); // Emit to all clients
+    io.emit('stateChange', switchState); // Emit to all connected clients
   });
 });
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
